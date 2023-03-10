a275b4470e2308f16b3aff5f5a30a67e4f8a28f1
Irina05-04 <79584806+Irina05-04@users.noreply.github.com>
2023-03-10 19:52:40 +0300

    feat: add extra tasks

---
+++ Extra tasks/2.2/main.js
@@ -0,0 +1,13 @@
+function broker(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length-1; i++){
+        if(arr[i]<arr[i+1])
+        sum += arr[i+1] - arr[i];
+    }
+    return sum;
+}
+console.log(broker([7,1,5,3,6,4])); //7
+console.log(broker([7,2,1,5,3,8])); //9
+console.log(broker([1,2,1,5,3,8])); //10
+console.log(broker([1,2,3,4,5]));  //4
+console.log(broker([7,6,4,3,1]))  //0
